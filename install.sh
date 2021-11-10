db4a9a949bedcbf11458e2cde38133ded7b26be4
Peter Rader <github@e-nexus.de>
2021-11-10 10:45:44 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,19 @@
-cd /root
+#!/bin/bash
+export old='`pwd`'
+export RDIR='/usr/share/ramdisk'
+echo -n "Where to install [`echo $RDIR`]: "
+read dir
+if [ "$dir" != "" ] ;then
+    export RDIR="$dir"
+fi
+
+if [ -d "$RDIR" ]; then
+  echo "Directory already exists! Cancel"
+  exit 1
+fi
+mkdir $RDIR
+cp * $RDIR
+cd $RDIR
 sudo chmod +x createramdisk openramdisk closeramdisk deleteramdisk
 sudo ./createramdisk
 sudo systemctl enable ramdisk.service
